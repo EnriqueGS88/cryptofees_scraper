c463514696f5a58bdb990efffcefeb058296fec6
Enrique G <enrique.gs@zoho.com>
2021-12-22 09:14:37 +0100

    export scrape to CSV

--- index_cryptofees.js
+++ index_cryptofees.js
@@ -1,14 +1,16 @@
 // Get all fees produced per protocol from cryptofees.info
 
 const puppeteer = require('puppeteer');
+const { convertArrayToCSV } = require('convert-array-to-csv');
+const converter = require('convert-array-to-csv');
+const fs = require('fs-extra');
 const listOfDates = require('./listOfDates');
 
 // URLs to scrape
 // const cryptofeesURL = `https://cryptofees.info/history/2021-11-02`;
 const cryptofeesURL = `https://cryptofees.info/history/`;
 
-// console.log(listOfDates[1]);
-
+const header = ['date', 'protocol', 'daily_fees_usd'];
 
 // Main Async function to scrape both sites
 
@@ -42,22 +44,28 @@ async function getPrices(){
 
     let historyPrices = [];
 
+    // Loop to get the protocol name and fees from 2 selectors
     for (let i = 0; i<listOfDates.length-1; i++) {
+        
+        await page.goto(cryptofeesURL.concat(listOfDates[i]));
+        await page.waitForSelector('div[class="jsx-2013905549 list"]');
 
         const nameSelector = `#__next > div > main > div.jsx-2013905549.list > a:nth-child(2) > div.jsx-166918656.name > div`;
         const feesSelector = `#__next > div > main > div.jsx-2013905549.list > a:nth-child(2) > div:nth-child(2)`;
 
-        await page.goto(cryptofeesURL.concat(listOfDates[i]));
-        await page.waitForSelector('div[class="jsx-2013905549 list"]');
 
         let date = listOfDates[i];
         let protocol = await page.$eval(nameSelector, element => element.innerText);
         let fees = await page.$eval(feesSelector, element => element.innerText);
+        let formatted_fees = fees.replaceAll(',', '').replace('$', '');
+        let fees_usd = formatted_fees.slice(0, (formatted_fees.length-3));
         let subArray = [];
+
+        console.log(fees_usd);
 
         subArray.push(date);
         subArray.push(protocol);
-        subArray.push(fees);
+        subArray.push(fees_usd);
 
         historyPrices.push(subArray);
 
@@ -65,7 +73,36 @@ async function getPrices(){
 
     }
 
-    console.log(historyPrices);
+    const csvFromArrayOfArrays = convertArrayToCSV(historyPrices, {
+        header,
+        separator: ','
+    });
+
+    // console.log(csvFromArrayOfArrays);
+    
+    async function example(f) {
+        try{
+            await fs.outputFile(f, csvFromArrayOfArrays);
+            const data = await fs.readFile(f, 'utf8');
+            console.log(data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const scrapeTime = new Date().toLocaleDateString()
+    .replaceAll('/', '')
+    .replaceAll(':', '')
+    .replaceAll(', ', '_');
+
+    let file = `./output/cryptofees_scrape_${scrapeTime}`;
+    example(file);
+
+
+
+
+
+
 
     /*
     debugger;
---
+++ stringslice.js
@@ -0,0 +1,5 @@
+let str = '37343967.71';
+
+let fee = str.slice(0, (str.length-3));
+
+console.log(fee);
