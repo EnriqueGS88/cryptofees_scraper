91756bc4fce493719d13f2b5a52d21846aa835d4
EnriqueGS88 <enrique.gzs@gmail.com>
2022-01-06 16:47:30 +0100

    cleansed script and modular scrapeData.js

--- convertArrayToJSON.js
+++ convertArrayToJSON.js
@@ -1,33 +1,10 @@
-const listOfDates = require('./listOfDates');
-
 // Convert Arrays to JSON with properties
+const listOfDates = require('./listOfDates');
+const scrapeData = require( './scrapeData' );
+
 const months = ["January", "February", "March", "April", "May", "June", "July"];
 const uniData = [0, 10, 15, 20, 25, 30, 35];
 const sushiData = [5, 15, 20, 17, 28, 29, 40];
-
-let scrapeData = [
-    [ '2021-12-31', 'Ethereum', '34347395' ],
-    [ '2021-12-31', 'Uniswap', '4027706' ],
-    [ '2021-12-31', 'Binance_Smart_Chain', '2574232' ],
-    [ '2021-12-31', 'SushiSwap', '1289752' ],
-    [ '2021-12-31', 'Aave', '1139513' ],
-    [ '2021-12-31', 'Bitcoin', '814037' ],
-    [ '2021-12-31', 'Trader_Joe', '796024' ],
-    [ '2021-12-31', 'Compound', '728653' ],
-    [ '2021-12-31', 'Balancer', '645590' ],
-    [ '2021-12-31', 'Quickswap', '423483' ],
-    [ '2021-12-31', 'Ethereum', '12345678' ],
-    [ '2021-12-31', 'Uniswap', '90123456' ],
-    [ '2021-12-31', 'Binance_Smart_Chain', '2512345' ],
-    [ '2021-12-31', 'SushiSwap', '1289752' ],
-    [ '2021-12-31', 'Aave', '1139513' ],
-    [ '2021-12-31', 'Bitcoin', '814037' ],
-    [ '2021-12-31', 'Trader_Joe', '796024' ],
-    [ '2021-12-31', 'Compound', '728653' ],
-    [ '2021-12-31', 'Balancer', '645590' ],
-    [ '2021-12-31', 'Quickswap', '423483' ],
-];
-
 const jsonFile = {
     "ethereumArray": [{
        date: '2021-12-29',
@@ -61,6 +38,7 @@ const jsonFile = {
      }]
 };
 
+// Split the array positions by the comma in the string
 let data = ["A123,G,2323232", null, "F345,G,345667", "T677,G,-34343", "G454,G,4343", ""],
 
 result = data
@@ -68,51 +46,23 @@ result = data
     .map(s => {
         let [UserId, Type, Values] = s.match(/[^,]+/g);
         return { UserId, Type, Values };
-});
-                
+});                
 // console.log(result);
 
-// let converted = scrapeData.map( s => {
-//     // let [date, protocol, fee] = s.match( /[^,]+/g );
-//     return { date: s, protocol: s[0][1], fee: s[0][2]};
-// });
 
-// console.log( scrapeData[0][1] );
-
-
+// Define structure for the output json
 let jsonOutput = {
     "ethereumData": [],
     "uniswapData": [],
     "binance_Smart_ChainData": [],
 };
 
+// List the buckets/protocols that will receive the mapped data
 let protocols = [
     'Ethereum',
     'Uniswap',
     'Binance_Smart_Chain',
 ]
-
-
-// let mapProtocolToJSON = ( data, row ) => {
-    
-//     for ( let k = 0; k < protocols.length; k++ ) {
-
-        
-//         if ( data[row][1] == protocols[k] ) {
-
-//             let object = { "date": data[row][0], "fee": data[row][2] };
-//             jsonOutput.protocols[k].push( object );
-            
-//         } else {
-            
-//             console.log( 'none' )
-            
-//         }
-//     }
-// }
-
-// mapProtocolToJSON( scrapeData, 10 );
-// console.log( jsonOutput )
 
 let mapProtocolToJSON = ( data, row ) => {
 
@@ -142,12 +92,10 @@ let mapProtocolToJSON = ( data, row ) => {
     }
 }
 
-
-
+// Evaluate each node in the scrapeData
+// Map it to the corresponding protocol array in the json output file
 for ( let r = 0; r < scrapeData.length ; r++ ) {
-    
-    mapProtocolToJSON( scrapeData, r )
-
+    mapProtocolToJSON( scrapeData, r );
 }
 
 console.log( jsonOutput );
